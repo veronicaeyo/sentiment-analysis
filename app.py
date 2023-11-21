85c56ec6b93a8220ee19491d5750b06b46bcc616
Abiodun Allison <allisonabiodun@gmail.com>
2023-11-21 17:20:17 +0100

    try MultinomialNB model

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ import streamlit as st
 import pickle
 from scripts.preprocess import preprocess
 
-with open('model/lr_model.pkl', 'rb') as f:
-    lr_model = pickle.load(f)
+with open("model/mnb_model.pkl", "rb") as f:
+    model = pickle.load(f)
 
 sentiment_dict = {1: "Depressed", 0: "Not Depressed"}
 
 st.title("Sentiment Analysis")
 
 
-try:    
-    text = st.text_input("Please enter your text", '')
+try:
+    text = st.text_input("Please enter your text", "")
     if st.button("Predict"):
         test_string = preprocess(text)
-        prediction = lr_model.predict(test_string)
+        prediction = model.predict(test_string)
 
-        prob = lr_model.predict_proba(test_string).max()
-        st.write(f"The tone of this sentence is: **{sentiment_dict.get(prediction[0])}**, with probability: {prob:.2f}")   
+        prob = model.predict_proba(test_string).max()
+        st.write(
+            f"The tone of this sentence is: **{sentiment_dict.get(prediction[0])}**, with probability: {prob:.2f}"
+        )
 except ValueError as e:
     st.write(str(e))
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,37 +1,43 @@
-
 import pickle
 import string
-import numpy as np 
+import numpy as np
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-nltk.download('punkt')
-nltk.download('stopwords') 
 
-nltk.download('wordnet') 
-nltk.download('omw-1.4')
+nltk.download("punkt")
+nltk.download("stopwords")
+
+nltk.download("wordnet")
+nltk.download("omw-1.4")
 lemmatizer = WordNetLemmatizer()
 
 
 with open("model/tf_idf.pkl", "rb") as f:
     lsa_idf = pickle.load(f)
 
-stop_words = stopwords.words('english') 
+stop_words = stopwords.words("english")
 punctuation_list = list(string.punctuation)
 useless_words = stop_words + punctuation_list
 
-def preprocess(text: str) ->np.array:
+
+def preprocess(text: str) -> np.ndarray:
     if text:
-        test_string = " ".join(word for word in word_tokenize(text.lower()) if word not in useless_words)
-        test_string = " ".join(lemmatizer.lemmatize(word) for word in word_tokenize(test_string))
+        test_string = " ".join(
+            word for word in word_tokenize(text.lower()) if word not in useless_words
+        )
+        test_string = " ".join(
+            lemmatizer.lemmatize(word) for word in word_tokenize(test_string)
+        )
         test_string_idf = lsa_idf.transform([test_string])
 
         return test_string_idf
     else:
         raise ValueError("Please enter a text")
 
-if __name__ == '__main__':
-    preprocess()
+
+if __name__ == "__main__":
+    preprocess() # type: ignore
